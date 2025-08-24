29dc7bbbfa89c32a0837cab6acfda427c8744587
Ravi Verma <44615530+raviverma9807@users.noreply.github.com>
2025-08-24 12:25:15 +0530

    updated program file

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Serve static files from project root
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory()))
+});
+
+// Serve index.html on all unmatched routes
+app.MapFallback(async context =>
+{
+    context.Response.ContentType = "text/html";
+    await context.Response.SendFileAsync(Path.Combine(Directory.GetCurrentDirectory(), "index.html"));
+});
 
 app.Run();
